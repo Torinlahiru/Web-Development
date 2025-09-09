9763e584231bf9d955327ced2694a91b979025a7
Torin Lahiru <torinlahiru@gmail.com>
2025-09-09 11:59:45 +0530

    time input  added with js

--- To Do App/js/script.js
+++ To Do App/js/script.js
@@ -1,12 +1,13 @@
 function addTask() {
   const input = document.getElementById("taskInput");
+  const inputTime = document.getElementById("toDoTime");
   const taskText = input.value.trim();
   if (taskText === "") return;
 
   const li = document.createElement("li");
   li.innerHTML = `
-        <p onclick="toggleTask(this)">${taskText}
-        </p>
+        <p >${taskText}</p>
+        <p >${inputTime.value}</p>
         <span class="delete" onclick="deleteTask(this)">×</span>
       `;
 
